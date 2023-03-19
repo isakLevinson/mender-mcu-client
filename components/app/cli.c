d722635d47c9f4142b6143ed07b11471f6b29810
isak <isak.levinson@gmail.com>
2023-03-19 02:17:18 +0200

    cli cleanups

--- components/app/cli.c
+++ components/app/cli.c
@@ -9,25 +9,11 @@
 
 #include <stdio.h>
 #include <string.h>
-#include "esp_log.h"
-#include "esp_console.h"
-#include "argtable3/argtable3.h"
-#include "cmd_decl.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
-#include "esp_wifi.h"
-#include "esp_netif.h"
-#include "esp_event.h"
-#include "iperf.h"
-#include "esp_coexist.h"
-
-#include <sys/socket.h>
-#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_check.h"
-#include "esp_log.h"
-#include "esp_rom_sys.h"
-#include "esp_timer.h"
+
+
 #include "iperf.h"
 
 #include "driver/uart.h"
@@ -89,39 +75,18 @@ static void _free(void* i_pBuf)
 static void _task(void *arg)
 {
     char c;
-//    char str[256];
     size_t length;
 
     PRINT("Ready.\n");
 
-//    strcpy(str, "\r\nCLI task Ready\r\n");
-//    uart_write_bytes(ECHO_UART_PORT_NUM, str, strlen(str));
-//    uart_write_bytes(CONFIG_ESP_CONSOLE_UART_NUM, str, strlen(str));
-
     while(true) {
-        uart_get_buffered_data_len(CONFIG_ESP_CONSOLE_UART_NUM, &length);
-
-        if (length > 1) {
-            length = 1;
-        }
-
-        if (!length) {
-            vTaskDelay(10);
-        } else {
-            length = uart_read_bytes(CONFIG_ESP_CONSOLE_UART_NUM, &c, length, 1);
-            if (length) {
-                //uart_write_bytes(CONFIG_ESP_CONSOLE_UART_NUM, &c, 1);
-                DBG_MENU_handler(c);
-            }
+        length = uart_read_bytes(CONFIG_ESP_CONSOLE_UART_NUM, &c, 1, 1);
+        if (length) {
+            DBG_MENU_handler(c);
         }
     }
 
     vTaskDelete(NULL);
-
-
-
-
-
 }
 
 static bool dbgVer(uint8_t argc, char** argv)
