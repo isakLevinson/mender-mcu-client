ab3520b41efc44f48c7c53aa1f679650b57f60a1
Isak Levinson <isak.levinson@gmail.com>
2025-05-02 00:36:48 +0300

    added init and connect

--- main/tls.c
+++ main/tls.c
@@ -11,13 +11,31 @@
 #include <sys/param.h>
 #include "esp_netif.h"
 #include "lwip/sockets.h"
+#include "mbedtls/platform.h"
 #include "mbedtls/net_sockets.h"
+#include "mbedtls/esp_debug.h"
+#include "mbedtls/ssl.h"
 #include "mbedtls/entropy.h"
 #include "mbedtls/ctr_drbg.h"
+#include "mbedtls/error.h"
+#ifdef CONFIG_MBEDTLS_SSL_PROTO_TLS1_3
+#include "psa/crypto.h"
+#endif
+#include "esp_crt_bundle.h"
+
 #include "cmd.h"
 #include "wifi.h"
 #include "config.h"
 #include "tls.h"
+
+#define WEB_SERVER "192.168.1.100"
+#define WEB_PORT	"2000"
+mbedtls_entropy_context entropy;
+mbedtls_ctr_drbg_context ctr_drbg;
+mbedtls_ssl_context ssl;
+mbedtls_x509_crt cacert;
+mbedtls_ssl_config conf;
+mbedtls_net_context server_fd;
 
 
 
@@ -30,6 +48,8 @@ static void _task(void* arg)
 	mbedtls_ssl_config conf;
     mbedtls_entropy_context entropy;
     mbedtls_ctr_drbg_context ctr_drbg;
+	mbedtls_x509_crt cacert;
+
 	const char *pers = "ssl_server";
 	uint8_t buf[2048];
 	int		len;
@@ -40,6 +60,7 @@ static void _task(void* arg)
     mbedtls_net_init(&client_fd);
     mbedtls_ssl_init(&ssl);
     mbedtls_ssl_config_init(&conf);
+	mbedtls_x509_crt_init(&cacert);
 
 	mbedtls_entropy_init(&entropy);
     mbedtls_ctr_drbg_init(&ctr_drbg);
@@ -54,11 +75,24 @@ static void _task(void* arg)
 
     if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
 		(const unsigned char *) pers, strlen(pers))) != 0) {
-			ERROR(" mbedtls_ctr_drbg_seed  %x\n", -ret);
+			ERROR("mbedtls_ctr_drbg_seed %x\n", -ret);
 			goto exit;
 		}
 
 	INFO("mbedtls_ctr_drbg_seed ok\n");
+/*
+	ret = esp_crt_bundle_attach(&conf);
+    if(ret < 0)
+    {
+        ERROR("esp_crt_bundle_attach -0x%x", -ret);
+        goto exit;
+    }
+*/
+	if((ret = mbedtls_ssl_set_hostname(&ssl, "server")) != 0)
+    {
+        ERROR("mbedtls_ssl_set_hostname -0x%x", -ret);
+        goto exit;
+    }	
 
     if ((ret = mbedtls_net_bind(&listen_fd, NULL, "1000", MBEDTLS_NET_PROTO_TCP)) != 0) {
         ERROR("mbedtls_net_bind %x\n", -ret);
@@ -70,6 +104,10 @@ static void _task(void* arg)
 			ERROR("mbedtls_ssl_config_defaults %x\n", -ret);
 			goto exit;
 	}
+
+    mbedtls_ssl_conf_authmode(&conf, MBEDTLS_SSL_VERIFY_OPTIONAL);
+    mbedtls_ssl_conf_ca_chain(&conf, &cacert, NULL);
+    
 
 	if ((ret = mbedtls_ssl_setup(&ssl, &conf)) != 0) {
         ERROR("mbedtls_ssl_setup %x\n", -ret);
@@ -94,9 +132,11 @@ static void _task(void* arg)
 
 		do {
 			ret = mbedtls_ssl_handshake(&ssl);
+			INFO("mbedtls_ssl_handshake -%x\n", -ret);
 		} while (ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE);
 
 		if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
+			WARN("mbedtls_ssl_handshake -%x\n", -ret);
 			continue;
 		}
 
@@ -134,8 +174,6 @@ static void _task(void* arg)
 				break;
 			}
 		}
-
-
 	}
 
 	exit:
@@ -146,16 +184,149 @@ static bool _init(void)
 {
 	int	ret;
 
+	#ifdef CONFIG_MBEDTLS_SSL_PROTO_TLS1_3
+    psa_status_t status = psa_crypto_init();
+    if (status != PSA_SUCCESS) {
+        ESP_LOGE(TAG, "Failed to initialize PSA crypto, returned %d", (int) status);
+        return;
+    }
+#endif
+
+    mbedtls_ssl_init(&ssl);
+    mbedtls_x509_crt_init(&cacert);
+    mbedtls_ctr_drbg_init(&ctr_drbg);
+    INFO("Seeding the random number generator\n");
+
+    mbedtls_ssl_config_init(&conf);
+
+    mbedtls_entropy_init(&entropy);
+    ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, NULL, 0);
+    if (ret) {
+        ERROR("mbedtls_ctr_drbg_seed %d\n", ret);
+        return false;
+    }
+
+    INFO("Attaching the certificate bundle...\n");
+
+    ret = esp_crt_bundle_attach(&conf);
+    if(ret < 0) {
+        ERROR("esp_crt_bundle_attach -0x%x", -ret);
+        return false;
+    }
+
+    INFO("Setting hostname for TLS session...\n");
+
+     /* Hostname set here should match CN in server certificate */
+    ret = mbedtls_ssl_set_hostname(&ssl, WEB_SERVER);
+	if (ret) {
+        ERROR("mbedtls_ssl_set_hostname returned -0x%x\n", -ret);
+        return false;
+    }
+
+    INFO("Setting up the SSL/TLS structure...\n");
+
+    ret = mbedtls_ssl_config_defaults(&conf, MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_STREAM, MBEDTLS_SSL_PRESET_DEFAULT);
+    if (ret) {
+        ERROR("mbedtls_ssl_config_defaults %d\n", ret);
+        return false;
+    }
+
+    mbedtls_ssl_conf_authmode(&conf, MBEDTLS_SSL_VERIFY_OPTIONAL);
+    mbedtls_ssl_conf_ca_chain(&conf, &cacert, NULL);
+    mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
+#ifdef CONFIG_MBEDTLS_DEBUG
+    mbedtls_esp_enable_debug_log(&conf, CONFIG_MBEDTLS_DEBUG_LEVEL);
+#endif
+
+    ret = mbedtls_ssl_setup(&ssl, &conf);
+    if (ret) {
+        ERROR("mbedtls_ssl_setup -0x%x", -ret);
+        return false;
+    }
+
+/*
 	ret = xTaskCreate(_task, "tls", 16384, NULL, 3, NULL);
 	if (ret != pdPASS) {
 		ERROR("create task failed\n");
 		return false;
 	}
+*/
 	return true;
 }
 
+static bool dbgInit(uint8_t argc, char** argv)
+{
+	_init();
+	return true;
+}
+
+static bool dbgConnect(uint8_t argc, char** argv)
+{
+	int ret;
+	int flags;
+	char buf[512];
+
+	mbedtls_net_init(&server_fd);
+
+	INFO("Connecting to %s:%s...", WEB_SERVER, WEB_PORT);
+
+	ret = mbedtls_net_connect(&server_fd, WEB_SERVER, WEB_PORT, MBEDTLS_NET_PROTO_TCP);
+	if (ret) {
+		ERROR("mbedtls_net_connect -%x", -ret);
+		return false;
+	}
+
+	INFO("Connected.\n");
+
+	mbedtls_ssl_set_bio(&ssl, &server_fd, mbedtls_net_send, mbedtls_net_recv, NULL);
+
+	INFO("Performing the SSL/TLS handshake...\n");
+
+	while ((ret = mbedtls_ssl_handshake(&ssl)) != 0) {
+		if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
+			ERROR("mbedtls_ssl_handshake -0x%x", -ret);
+			return false;
+		}
+	}
+
+	INFO("Verifying peer X.509 certificate...\n");
+
+	if ((flags = mbedtls_ssl_get_verify_result(&ssl)) != 0) {
+		/* In real life, we probably want to close connection if ret != 0 */
+		WARN("Failed to verify peer certificate!\n");
+		bzero(buf, sizeof(buf));
+		mbedtls_x509_crt_verify_info(buf, sizeof(buf), "  ! ", flags);
+		WARN("verification info: %s\n", buf);
+	}
+	else {
+		INFO("Certificate verified.\n");
+	}
+
+	INFO("Cipher suite is %s\n", mbedtls_ssl_get_ciphersuite(&ssl));
+
+	return true;
+}
+
+
+static bool dbgStatus(uint8_t argc, char** argv)
+{
+	return true;
+}
+
+// *INDENT-OFF*
+DEBUG_MENU_START(g_menu)
+	DEBUG_MENU_DIR("tls", NULL)
+		DEBUG_MENU_CMD("status",  NULL,		NULL, dbgStatus)
+		DEBUG_MENU_CMD("init",	  NULL,		NULL, dbgInit)
+		DEBUG_MENU_CMD("connect", NULL,		NULL, dbgConnect)
+	DEBUG_MENU_DIR_END
+DEBUG_MENU_END
+// *INDENT-ON*
+
 bool TLS_init(void)
 {
+	DBG_TREE_add("/", g_menu);
+
 	_init();
 	return true;
 }
