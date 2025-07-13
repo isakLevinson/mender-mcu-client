dabe3f773985792829613ab39d0c83efaeccd4fa
Isak <isak.levinson@gmail.com>
2025-07-13 11:21:36 +0300

    removed mender default host and token

--- main/config.h
+++ main/config.h
@@ -2,9 +2,6 @@
 
 #include <sys_def.h>
 
-#define CONFIG_MENDER_SERVER_HOST "https://eu.hosted.mender.io"
-#define CONFIG_MENDER_SERVER_TENANT_TOKEN "RkxW6DPOFzxBWE2LR2o8muCbCQ2dRRQCDcmwXJRNuno"
-
 // *INDENT-OFF*
 #define CFG_LIST(cmd)	\
 	cmd(ssid,			cfg,	NULL)	\
